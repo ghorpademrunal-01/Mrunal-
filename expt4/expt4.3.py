ffcd15a62d88a449a02d8da11cea1aa21394f8d3
ghorpademrunal-01 <ghorpademrunal01@gmail.com>
2026-03-16 11:04:37 +0530

    Add files via upload

---
+++ expt4/expt4.3.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 16 05:33:51 2026
+
+@author: User
+"""
+
+# Reverse a customer feedback message.
+"""
+Created on Mon Mar  9 15:46:28 2026
+
+@author: priti
+"""
+
+feedback = "Quick delivery"
+
+# join() merges the characters back together
+reversed_feedback = "".join(reversed(feedback))
+
+print(reversed_feedback)
