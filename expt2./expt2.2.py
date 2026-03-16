9b3bf3f6d1a6b223035524c2a1b899b8eda8f9b0
ghorpademrunal-01 <ghorpademrunal01@gmail.com>
2026-03-16 10:37:18 +0530

    Add files via upload

---
+++ expt2./expt2.2.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 16 05:03:30 2026
+
+@author: User
+"""
+
+# Movie ticket eligibility
+"""
+Created on Tue Feb 10 13:04:27 2026
+
+@author: suyash
+"""
+age=int(input("Enter age:"))
+if (age>=18):
+    print("You are eligible")
+else:
+    print("You are not eligible")
