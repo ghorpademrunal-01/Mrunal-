741b74b1defbe22477a36c2d5caef3635ea14843
ghorpademrunal-01 <ghorpademrunal01@gmail.com>
2026-03-30 10:18:34 +0530

    Add files via upload

---
+++ Expt 6/exp6 act3.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 30 10:14:54 2026
+
+@author: User mrunal ghorpade
+"""
+
+speed = int(input("Enter vehicle speed: "))
+
+if speed > 60:
+    print("Overspeeding! You have to pay a fine.")
+else:
+    print("Speed is within the limit.")
